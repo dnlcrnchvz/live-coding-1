c3328ee59439555a92b97b1602406dd20a414faa
Daniela Ceron <dceronchavez09@gmail.com>
2024-10-30 15:05:19 -0600

    live coding

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí.
+/* `bigWords('bocina', myArray);` */
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio', 'zapato', 'celular', 'libreta', 'lapiz'];
+
+function imprimirArray(){
+    const changeElement = (array) =>{
+        const listItem = document.querySelectorAll(".item");
+        for(let i=0; i<myArray.length; i++){
+                listItem[i].textContent = array[i];
+        }
+    };
+    changeElement(myArray); 
+}
+imprimirArray();
